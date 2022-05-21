8e647e1c1987bf23fd19733d4cb42543b08d1336
petushka1 <nadya@devery.io>
2022-05-21 05:15:20 +0300

    production version

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
 // Mobile Menu and Layout
 const hmbrg = document.getElementById('hmbrg');
-const mobMenu = document.querySelector('.menuList');
-const menuDesktop = document.querySelector('.menuListDesktop');
 const close = document.getElementById('close');
 const logo = document.getElementById('logo');
+const about = document.getElementById('aboutLink');
+const home = document.getElementById('homeLink');
+const seeMore = document.getElementById('more');
+const seeLess = document.getElementById('less');
+
+const mobMenu = document.querySelector('.menuList');
+const menuDesktop = document.querySelector('.menuListDesktop');
 const topMenuDesktop = document.querySelector('.menuTop');
 const topMenuMob = document.querySelector('.topMob');
 
-const home = document.getElementById('homeLink');
-const about = document.getElementById('aboutLink');
+const page = document.querySelector('.page');
+const aboutPage = document.getElementById('about');
 const footer = document.getElementById('footer');
 const partners = document.getElementById('partners');
-const aboutPage = document.getElementById('about');
-
 const logoWhite = document.querySelector('#footer img');
 
 showFooter();
 colorFooter();
 window.addEventListener('resize', showFooter);
 window.addEventListener('resize', colorFooter);
+window.addEventListener('resize', showParticipants);
 
 close.addEventListener('click', closeMenu);
 hmbrg.addEventListener('click', openMenu);
 home.addEventListener('click', closeMenu);
 about.addEventListener('click', closeMenu);
 
-home.href = '#home';
-about.href = 'about.html';
+seeLess.style.display = 'none';
 
-/* Dynamic Section */
+if (page.id === 'about' && window.innerWidth >= 768) {
+  about.style.display = 'none';
+  home.style.display = 'block';
+}
+
+/* Dynamic Participants Section */
 const cardsArr = [
   {
     image: 'img/photo1.png',
@@ -40,7 +48,7 @@ const cardsArr = [
   {
     image: 'img/photo2.png',
     alt: 'Ethereum',
-    name: 'Vitalik" Buterin',
+    name: 'Vitalik Buterin',
     position: 'Co-founders of Ethereum. Canadian programmer and writer',
     info: 'Mauris ut mauris fringilla, placerat dolor id, condimentum urna. Duis interdum arcu ut consectetur porttitor.'
   },
@@ -52,50 +60,88 @@ const cardsArr = [
     info: 'Mauris ut mauris fringilla, placerat dolor id, condimentum urna. Duis interdum arcu ut consectetur porttitor.'
   },
   {
-    image: 'img/photo1.png',
-    alt: 'First Speaker',
-    name: 'Brad Garlinghouse ',
-    position: 'CEO of financial technology company Ripple Labs',
+    image: 'img/photo4.png',
+    alt: 'Nike',
+    name: 'John Donahoe',
+    position: 'CEO of Nike and American businessman',
     info: 'Mauris ut mauris fringilla, placerat dolor id, condimentum urna. Duis interdum arcu ut consectetur porttitor.'
   },
   {
-    image: 'img/photo1.png',
-    alt: 'First Speaker',
-    name: 'Brad Garlinghouse ',
-    position: 'CEO of financial technology company Ripple Labs',
+    image: 'img/photo5.png',
+    alt: 'Saachi',
+    name: 'Kevin Roberts',
+    position: 'Former executive chairman of Saatchi & Saatchi and speaker',
     info: 'Mauris ut mauris fringilla, placerat dolor id, condimentum urna. Duis interdum arcu ut consectetur porttitor.'
   },
   {
-    image: 'img/photo1.png',
-    alt: 'First Speaker',
-    name: 'Brad Garlinghouse ',
-    position: 'CEO of financial technology company Ripple Labs',
+    image: 'img/photo6.png',
+    alt: 'Open Sea',
+    name: 'Devin Finzer',
+    position: 'Co-founder and CEO at OpenSea ',
     info: 'Mauris ut mauris fringilla, placerat dolor id, condimentum urna. Duis interdum arcu ut consectetur porttitor.'
   },
 ];
 
 const cardsSection = document.querySelector('.gridParticipants');
 var cardElement = '';
-
-
-function createCard(arr) {
-for (let i = 0; i < arr.length; i++) {
-cardElement +=
-`<div class="card">
-  <div class="border">
-    <img src=${arr[i].image} alt=${arr[i].altText}>
-  </div>
-  <div class="descriptionWrapper">
-    <h3 class="bold marginTop">${arr[i].name}</h3>
-    <p class="description marginTop">${arr[i].position}</p>
-    <p class="lineGray"></p>
-    <p class="update">${arr[i].info}</p>
-  </div>
-  </div>`;
+var cardElementHidden = '';
+for (let i = 0; i < cardsArr.length; i++) {
+  if (i <= 1) {
+    cardElement +=
+    `<div class="card ${i > 1 ? "unseen" : ""}">
+      <div class="border">
+        <img src=${cardsArr[i].image} alt=${cardsArr[i].altText}>
+      </div>
+      <div class="descriptionWrapper">
+        <h3 class="bold marginTop">${cardsArr[i].name}</h3>
+        <p class="description marginTop">${cardsArr[i].position}</p>
+        <p class="lineGray"></p>
+        <p class="update">${cardsArr[i].info}</p>
+      </div>
+      </div>`;
   }
-  cardsSection.innerHTML = cardElement;
+  else {
+    cardElementHidden +=
+    `<div class="card ${i > 1 ? "unseen" : ""}">
+      <div class="border">
+        <img src=${cardsArr[i].image} alt=${cardsArr[i].altText}>
+      </div>
+      <div class="descriptionWrapper">
+        <h3 class="bold marginTop">${cardsArr[i].name}</h3>
+        <p class="description marginTop">${cardsArr[i].position}</p>
+        <p class="lineGray"></p>
+        <p class="update">${cardsArr[i].info}</p>
+      </div>
+      </div>`;
+    }
+  }
+
+
+
+
+showParticipants();
+
+function showParticipants() {
+  if (window.innerWidth >= 768) {
+    cardsSection.innerHTML = cardElement.concat(cardElementHidden);
+  }
+  else {
+    cardsSection.innerHTML = cardElement;
+  }
 }
-createCard(cardsArr);
+
+function showMore() {
+  cardsSection.innerHTML = cardElement.concat(cardElementHidden);
+  seeMore.style.display = 'none';
+  seeLess.style.display = 'block';
+}
+
+function showLess() {
+  cardsSection.innerHTML = cardElement;
+  seeMore.style.display = 'block';
+  seeLess.style.display = 'none';
+}
+
 
 function showFooter() {
   if (window.innerWidth >= 768) {
@@ -125,8 +171,10 @@ function openMenu() {
 }
 
 function closeMenu() {
-  mobMenu.style.display = 'none';
-  topMenuMob.style.display = 'none';
+  if (window.innerWidth < 768) {
+    mobMenu.style.display = 'none';
+    topMenuMob.style.display = 'none';
+  }
 }
 
 function colorFooter() {
@@ -141,3 +189,6 @@ function colorFooter() {
     logoWhite.src = 'img/logo.svg';
   }
 }
+
+seeMore.addEventListener('click', showMore);
+seeLess.addEventListener('click', showLess);
